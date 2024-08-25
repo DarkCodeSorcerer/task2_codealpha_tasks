bb28272667b54fbf0ae43ec2c1a5eac85ae790c9
Farshad Inayathulla <143897486+DarkCodeSorcerer@users.noreply.github.com>
2024-08-25 23:33:10 +0530

    Web Application

---
+++ My-Music/src/components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className="absolute bg-black bottom-0 flex flex-col items-center py-6 text-white w-full">
+			Copyright &copy; Music Master | Create By Farshad 2024
+		</footer>
+	);
+};
+
+export default Footer;
